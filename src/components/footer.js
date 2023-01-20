1eaa8c31152a065e8b6eadddaa9dca6cb746161a
Fraz Khan <fraz9838@gmail.com>
2023-01-20 20:07:14 +0500

    7th

--- src/components/footer.js
+++ src/components/footer.js
@@ -7,10 +7,6 @@ function Footer() {
       <div class="container fixed-bottom">
         <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
           <div class="col-md-12 d-flex align-items-center">
-            <a
-              href="/"
-              class="mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1"
-            ></a>
             <div class="text-muted d-flex align-items-center justfiy-content-center">
               <div className="px-3">
                 <lottie-player
