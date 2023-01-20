383d9571ef87a47944ecc51de078fbd526562560
Fraz Khan <fraz9838@gmail.com>
2023-01-20 19:19:59 +0500

    4th

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import "./style.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
